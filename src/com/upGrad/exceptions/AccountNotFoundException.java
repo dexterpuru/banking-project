1ce46adb4ed21ffc71332b42aa576e901e7d4705
sndpoffcl <sandeep2official@gmail.com>
2020-06-29 04:43:22 +0530

    clearing old code

--- src/com/upGrad/exceptions/AccountNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.upGrad.exceptions;
-
-public class AccountNotFoundException extends Exception {
-    public AccountNotFoundException(String s){
-        //call constructor of parent exception
-        super(s);
-    }
-}
